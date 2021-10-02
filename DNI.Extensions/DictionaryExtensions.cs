cfe7b27cb98bc9828b67d5670974ea696e36df57
RomeshAbeyawardena <romesh.abeyawardena@concerto.co.uk>
2021-10-02 21:53:02 +0100

    It works woohoo

--- DNI.Extensions/DictionaryExtensions.cs
+++ DNI.Extensions/DictionaryExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DNI.Shared.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,16 @@ using System.Threading.Tasks;
 
 namespace DNI.Extensions
 {
-    public class DictionaryExtensions
+    public static class DictionaryExtensions
     {
-        
+        public static void AddRange<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, IEnumerable<KeyValuePair<TKey, TValue>> items)
+        {
+            items.ForEach(item => dictionary.Add(item));
+        }
+
+        public static void AddRange<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, IEnumerable<IKeyValuePair<TKey, TValue>> items)
+        {
+            AddRange(dictionary, items.Select(i => i.KeyValuePair));
+        }
     }
 }
--- Extensions/MigrationManager/DNI.MigrationManager.Core/Defaults/DefaultMigrationManager.cs
+++ Extensions/MigrationManager/DNI.MigrationManager.Core/Defaults/DefaultMigrationManager.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DNI.Extensions;
 
 namespace DNI.MigrationManager.Core.Defaults
 {
