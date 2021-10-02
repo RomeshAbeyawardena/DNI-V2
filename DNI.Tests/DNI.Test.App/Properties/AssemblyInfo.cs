0fb4f2fc41315cbedfacb95beab89909f035c034
RomeshAbeyawardena <romesh.abeyawardena@concerto.co.uk>
2021-10-02 22:40:48 +0100

    ta

---
+++ DNI.Tests/DNI.Test.App/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Test.App.Properties
+{
+    public class AssemblyInfo
+    {
+        
+    }
+}
