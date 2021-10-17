7835058004a5b683a4d87652521705545983c3d1
RomeshAbeyawardena <romesh.abeyawardena@concerto.co.uk>
2021-10-17 16:39:57 +0100

    Simplified extension method

--- Extensions/Modules/DNI.Modules.Core/Extensions/ModuleConfigurationExtensions.cs
+++ Extensions/Modules/DNI.Modules.Core/Extensions/ModuleConfigurationExtensions.cs
@@ -12,38 +12,11 @@ namespace DNI.Modules.Core.Extensions
     {
         public static bool ApplyConfiguration(this IModuleConfiguration moduleConfiguration, IModuleConfigurationBuilder moduleConfigurationBuilder)
         {
-            bool hasChanged = false;
-
-            void SetHasChangedFlag()
-            {
-                if(!hasChanged)
-                    hasChanged = true;
-            }
             var ct = moduleConfiguration.ModuleTypes.Count();
-            var newModuleConfiguration = moduleConfigurationBuilder.Build(moduleConfiguration.ServiceProvider);
+            moduleConfigurationBuilder.Build(moduleConfiguration.ServiceProvider);
             var newct = moduleConfiguration.ModuleTypes.Count();
 
-            hasChanged = ct != newct;
-
-            foreach (var moduleType in newModuleConfiguration.ModuleTypes)
-            {
-                if (!moduleConfiguration.ModuleTypes.Contains(moduleType))
-                {
-                    moduleConfiguration.ModuleTypes = moduleConfiguration.ModuleTypes.Append(moduleType);
-                    SetHasChangedFlag();
-                }
-            }
-
-            foreach (var (t, o) in newModuleConfiguration.Options)
-            {
-                if (!moduleConfiguration.Options.ContainsKey(t))
-                {
-                    moduleConfiguration.Options.Add(t, o);
-                    SetHasChangedFlag();
-                }
-            }
-
-            return hasChanged;
+            return ct != newct;
         }
     }
 }
