edca638b2b2525471db58ccc455cde51e439eae1
romesh-abeyawardena <romesh.abeyawardena@concerto.co.uk>
2021-10-18 10:12:09 +0100

    Added base controllers

---
+++ Extensions/Web/DNI.Web.Shared/Base/ApiControllerBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Web.Shared.Base
+{
+    [Route("api/[version]/{controller}")]
+    [ApiVersion("1.0.0")]
+    public abstract class ApiVersionControllerBase : ControllerBase
+    {
+        
+    }
+
+    [Route("api/{controller}")]
+    public abstract class ApiControllerBase : ControllerBase
+    {
+
+    }
+}
