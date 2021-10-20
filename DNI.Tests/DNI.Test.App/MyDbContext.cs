d0f08b8a1061f5c04ec50aba2808e1eaad6d7127
RomeshAbeyawardena <romesh.abeyawardena@concerto.co.uk>
2021-10-20 22:05:53 +0100

    - Fixed issue by copying services

---
+++ DNI.Tests/DNI.Test.App/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DNI.Data.Shared.Base;
+using DNI.Shared.Test;
+using Microsoft.EntityFrameworkCore;
+
+namespace DNI.Test.App
+{
+    class MyDbContext : DbContextBase
+    {
+        public MyDbContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
