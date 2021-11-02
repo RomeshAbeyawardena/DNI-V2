55d091d9845e07b37e93a0b68748bc1d81142ae9
romesh-abeyawardena <romesh.abeyawardena@concerto.co.uk>
2021-11-02 11:45:42 +0000

    Moved to new internal partial class

---
+++ Extensions/Hangfire/DNI.Hangfire.Modules/HangfireWebModule.Internals.cs
@@ -0,0 +1,44 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Hangfire.Modules
+{
+    public partial class HangfireWebModule
+    {
+        private void ConfigureEndpoints(IEndpointRouteBuilder endpointRouteBuilder)
+        {
+            if (Options.UseHangfireDashboard)
+            {
+                endpointRouteBuilder.MapHangfireDashboard();
+            }
+        }
+
+        private void ConfigureAppBuilder(IApplicationBuilder applicationBuilder)
+        {
+            if (Options.UseHangfireDashboard)
+            {
+                applicationBuilder
+                    .UseHangfireDashboard(Options.PathMatch ?? "/hangfire", Options.DashboardOptions, Options.JobStorage);
+            }
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddHangfire(c => ConfigureService(c))
+                .AddHangfireServer();
+        }
+
+        private void ConfigureService(IGlobalConfiguration configuration)
+        {
+            Options.ConfigureHangfire(configuration);
+        }
+    }
+}
--- Extensions/Hangfire/DNI.Hangfire.Modules/HangfireWebModule.cs
+++ Extensions/Hangfire/DNI.Hangfire.Modules/HangfireWebModule.cs
@@ -4,14 +4,11 @@ using DNI.Modules.Shared.Abstractions;
 using DNI.Modules.Shared.Abstractions.Builders;
 using DNI.Modules.Shared.Base;
 using DNI.Web.Modules.Extensions;
-using Hangfire;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DNI.Hangfire.Modules
 {
-    public class HangfireWebModule : ModuleBase
+    public partial class HangfireWebModule : ModuleBase
     {
         private readonly IModuleConfiguration moduleConfiguration;
 
@@ -26,38 +23,10 @@ namespace DNI.Hangfire.Modules
                     .ConfigureEndpoints(ConfigureEndpoints));
         }
 
-        private void ConfigureEndpoints(IEndpointRouteBuilder endpointRouteBuilder)
-        {
-            if (Options.UseHangfireDashboard)
-            {
-                endpointRouteBuilder.MapHangfireDashboard();
-            }
-        }
-
-        private void ConfigureAppBuilder(IApplicationBuilder applicationBuilder)
-        {
-            if (Options.UseHangfireDashboard)
-            {
-                applicationBuilder
-                    .UseHangfireDashboard(Options.PathMatch ?? "/hangfire", Options.DashboardOptions, Options.JobStorage);
-            }
-        }
-
         public HangfireWebModule(IModuleConfiguration moduleConfiguration)
         {
             this.moduleConfiguration = moduleConfiguration;
         }
 
-        private void ConfigureServices(IServiceCollection services)
-        {
-            services
-                .AddHangfire(c => ConfigureService(c))
-                .AddHangfireServer();
-        }
-
-        private void ConfigureService(IGlobalConfiguration configuration)
-        {
-            Options.ConfigureHangfire(configuration);
-        }
     }
 }
