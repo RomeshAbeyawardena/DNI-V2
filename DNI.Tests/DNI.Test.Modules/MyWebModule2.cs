1f30f615442836282f0d303e093ba1885ac4062f
romesh-abeyawardena <romesh.abeyawardena@concerto.co.uk>
2021-11-02 15:14:17 +0000

    Added test module using hang fire

--- DNI.Tests/DNI.Test.Modules/MyWebModule2.cs
+++ DNI.Tests/DNI.Test.Modules/MyWebModule2.cs
@@ -6,11 +6,21 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System.Net;
 using Hangfire;
+using System;
+using Hangfire.SqlServer;
+using Microsoft.Extensions.Configuration;
 
 namespace DNI.Test.Modules
 {
     public class MyWebModule2 : ModuleBase
     {
+        private readonly IConfiguration configuration;
+
+        public MyWebModule2(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public override void ConfigureModuleBuilder(IServiceCollection services, IModuleConfigurationBuilder moduleConfigurationBuilder)
         {
             
@@ -19,7 +29,22 @@ namespace DNI.Test.Modules
                 .ConfigureHangfireModule<MyWebModule2>(options => options
                     .UseHangfireDashboard()
                     .ConfigureOptions("/hangfire", new DashboardOptions(), null)
+                    .ConfigureHangfire(Configure)
                     .ConfigureWebHost(ConfigureHangFire));
+        }
+
+        private void Configure(IGlobalConfiguration configuration)
+        {
+            configuration.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+            .UseSimpleAssemblyNameTypeSerializer()
+            .UseRecommendedSerializerSettings()
+            .UseSqlServerStorage(this.configuration.GetConnectionString("hangfire"), new SqlServerStorageOptions
+            {
+                CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                QueuePollInterval = TimeSpan.Zero,
+                UseRecommendedIsolationLevel = true
+            });
         }
 
         private void ConfigureHangFire(IWebHostBuilder webHostBuilder)
